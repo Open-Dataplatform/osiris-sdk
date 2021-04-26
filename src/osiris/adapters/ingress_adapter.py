b69b6fcff7c115bd2f80dad91d4d92d323c32346
Araz <68658118+araz-code@users.noreply.github.com>
2021-04-26 12:42:08 +0200

    Update ingress_adapter.py
    Fixed bugs in imports

--- src/osiris/adapters/ingress_adapter.py
+++ src/osiris/adapters/ingress_adapter.py
@@ -7,7 +7,7 @@ import sys
 from datetime import datetime
 from io import BytesIO
 
-from src.osiris.apis.ingress import Ingress
+from ..apis.ingress import Ingress
 
 
 logger = logging.getLogger(__name__)
