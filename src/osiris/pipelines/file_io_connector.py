2331831066c7340fedd57cf1b10131c135f2e107
Araz B. Makoo <araz100@gmail.com>
2021-05-27 13:32:22 +0200

    added state to datalakesource

--- src/osiris/pipelines/file_io_connector.py
+++ src/osiris/pipelines/file_io_connector.py
@@ -2,12 +2,14 @@
 A source to download files from Azure Datalake
 """
 import os
+import pandas as pd
+import json
 from datetime import timedelta, datetime
-from typing import List, Optional, Generator
+from typing import List, Optional, Generator, Dict
 
 from apache_beam.io import OffsetRangeTracker, iobase
 from azure.core.exceptions import ResourceNotFoundError
-from azure.storage.filedatalake import DataLakeServiceClient, DataLakeFileClient, FileSystemClient, PathProperties
+from azure.storage.filedatalake import DataLakeFileClient, FileSystemClient, PathProperties
 
 from ..core.azure_client_authorization import AzureCredential
 
@@ -16,32 +18,97 @@ class DatalakeFileSource(iobase.BoundedSource):  # noqa
     """
     A Class to download files from Azure Datalake
     """
+
+    STATE_FILE = 'transformation_state.json'
+
     # pylint: disable=too-many-arguments
     def __init__(self,
-                 ingest_time: datetime,
                  credential: AzureCredential,
                  account_url: str,
                  filesystem_name: str,
-                 guid: str):
+                 guid: str,
+                 ingest_time: datetime = None,
+                 max_files: int = 10):
+
+        if None in [credential, account_url, filesystem_name, guid]:
+            raise TypeError
+
+        self.credential = credential
         self.account_url = account_url
         self.filesystem_name = filesystem_name
-        self.credential = credential
-        self.file_paths = self.__get_file_paths(ingest_time, guid)
+        self.guid = guid
+        self.ingest_time = ingest_time
+        self.max_files = max_files
 
-    def __get_file_paths(self, ingest_time: datetime, guid: str) -> List[PathProperties]:
-        folder_path1 = f'{guid}/year={ingest_time.year}/month={ingest_time.month:02d}/day={ingest_time.day:02d}' + \
-                       f'/hour={ingest_time.hour:02d}'
+        self.file_paths = self.__get_file_paths()
 
-        prev_hour = ingest_time - timedelta(hours=1)
-        folder_path2 = f'{guid}/year={prev_hour.year}/month={prev_hour.month:02d}/day={prev_hour.day:02d}' + \
+    def __get_file_paths(self) -> List[PathProperties]:
+        with FileSystemClient(self.account_url, self.filesystem_name,
+                              credential=self.credential) as filesystem_client:
+
+            if self.ingest_time is None:
+                return self.__get_paths_since_last_run(filesystem_client)
+
+            return self.__get_paths_on_basis_of_ingest_time(filesystem_client)
+
+    def __get_paths_on_basis_of_ingest_time(self, filesystem_client) -> List[PathProperties]:
+        folder_path1 = f'{self.guid}/year={self.ingest_time.year}/month={self.ingest_time.month:02d}/' + \
+                       f'day={self.ingest_time.day:02d}/hour={self.ingest_time.hour:02d}'
+
+        prev_hour = self.ingest_time - timedelta(hours=1)
+        folder_path2 = f'{self.guid}/year={prev_hour.year}/month={prev_hour.month:02d}/day={prev_hour.day:02d}' + \
                        f'/hour={prev_hour.hour:02d}'
 
         paths = []
-        with DataLakeServiceClient(self.account_url, credential=self.credential) as service_client:
-            file_system_client = service_client.get_file_system_client(self.filesystem_name)
-            paths += self.__get_file_paths_from_folder(folder_path1, file_system_client)
-            paths += self.__get_file_paths_from_folder(folder_path2, file_system_client)
-        return paths
+        paths += self.__get_file_paths_from_folder(folder_path1, filesystem_client)
+        paths += self.__get_file_paths_from_folder(folder_path2, filesystem_client)
+
+        return paths[:self.max_files]
+
+    def __get_paths_since_last_run(self, filesystem_client: FileSystemClient) -> List[PathProperties]:
+        state = self.__retrieve_transformation_state(filesystem_client)
+
+        if not state:
+            now = datetime.utcnow()
+            folder_path = f'{self.guid}/year={now.year}/month={now.month:02d}/day={now.day:02d}/hour={now.hour:02d}'
+
+            paths = self.__get_file_paths_from_folder(folder_path, filesystem_client)
+
+            return paths[:self.max_files]
+
+        else:
+            last_successful_run = datetime.strptime(state['last_successful_run'], '%Y-%m-%dT%H:%M:%S')
+            now = datetime.utcnow()
+            time_range = pd.date_range(last_successful_run, now, freq='H')
+
+            paths = []
+            for timeslot in time_range:
+                folder_path = f'{self.guid}/year={timeslot.year}/month={timeslot.month:02d}/day={timeslot.day:02d}' + \
+                              f'/hour={timeslot.hour:02d}'
+
+                temp_paths = self.__get_file_paths_from_folder(folder_path, filesystem_client)
+
+                for path in temp_paths:
+                    if path.last_modified > last_successful_run:
+                        paths.append(path)
+
+                if len(paths) > self.max_files:
+                    break
+
+        return paths[:self.max_files]
+
+    def __retrieve_transformation_state(self, filesystem_client: FileSystemClient) -> Optional[Dict]:
+        with filesystem_client.get_file_client(f'{self.guid}/{self.STATE_FILE}') as file_client:
+            try:
+                state = file_client.download_file().readall()
+                return json.loads(state)
+            except ResourceNotFoundError:
+                return None
+
+    def __save_transformation_state(self, filesystem_client: FileSystemClient, state: Dict):
+        with filesystem_client.get_file_client(f'{self.guid}/{self.STATE_FILE}') as file_client:
+            json_data = json.dumps(state)
+            file_client.upload_data(json_data, overwrite=True)
 
     @staticmethod
     def __get_file_paths_from_folder(folder_path: str, file_system_client: FileSystemClient) -> List[PathProperties]:
@@ -71,8 +138,7 @@ class DatalakeFileSource(iobase.BoundedSource):  # noqa
         """
         Returns the content of the next file
         """
-        for i in range(range_tracker.start_position(),
-                       range_tracker.stop_position()):
+        for i in range(range_tracker.start_position(), range_tracker.stop_position()):
             if not range_tracker.try_claim(i):
                 return
 
@@ -107,6 +173,39 @@ class DatalakeFileSource(iobase.BoundedSource):  # noqa
                 stop_position=bundle_stop)
             bundle_start = bundle_stop
 
+    def close(self):
+        """
+        Updates the transformation state file after a successful run. Its important this method gets called
+        after the pipeline has run or else the datasource will keep processing already processed files.
+        """
+
+        # If ingest time its set, we will not update or use the state. Close() should not be called in this
+        # situation.
+        if self.ingest_time:
+            return
+
+        with FileSystemClient(self.account_url, self.filesystem_name,
+                              credential=self.credential) as filesystem_client:
+            state = self.__retrieve_transformation_state(filesystem_client)
+
+            # state file doesn't exist. We create a fresh one.
+            if not state:
+                state = {}
+
+            if len(self.file_paths):
+                latest_modified = self.__get_latest_modified(self.file_paths)
+                state['last_successful_run'] = latest_modified.isoformat()
+                self.__save_transformation_state(filesystem_client, state)
+
+    @staticmethod
+    def __get_latest_modified(file_paths):
+        latest = file_paths[0]
+        for file_path in file_paths[1:]:
+            if file_path.last_modified > latest:
+                latest = file_path.last_modified
+
+        return latest
+
 
 class DatalakeFileSourceWithFileName(DatalakeFileSource):  # noqa
     """
@@ -116,8 +215,7 @@ class DatalakeFileSourceWithFileName(DatalakeFileSource):  # noqa
         """
         Returns the content of the next file.
         """
-        for i in range(range_tracker.start_position(),
-                       range_tracker.stop_position()):
+        for i in range(range_tracker.start_position(), range_tracker.stop_position()):
             if not range_tracker.try_claim(i):
                 return
 
