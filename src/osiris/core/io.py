f6f32b38986822c7996c25e9b27c910b759009a4
runethorbek <runethorbek@gmail.com>
2021-06-25 10:48:12 +0200

    added OsirisFileClients + some necessary tox updates

--- src/osiris/core/io.py
+++ src/osiris/core/io.py
@@ -2,8 +2,17 @@
 Contains Osiris common IO functions
 """
 from datetime import datetime
+from typing import Any, Optional, Union, AnyStr, Iterable, IO, Dict
+
+from azure.storage.blob import ContentSettings, StorageStreamDownloader
+from azure.storage.blob.aio import StorageStreamDownloader as StorageStreamDownloaderAsync
+from azure.storage.filedatalake.aio import DataLakeFileClient as DataLakeFileClientAsync
+from azure.storage.filedatalake import DataLakeFileClient
+from prometheus_client import CollectorRegistry, Counter, push_to_gateway
+
 import pandas as pd
 
+from .configuration import ConfigurationWithCredentials
 from .enums import TimeResolution
 
 
@@ -56,3 +65,169 @@ def parse_date_str(date_str):
         raise ValueError('Wrong string format for date')
     except ValueError as error:
         raise ValueError('Wrong string format for date: ', error) from error
+
+
+def singleton(cls, *args, **kw):
+    """
+    Function needed to create a singleton of a class
+    """
+    instances = {}
+
+    def _singleton():
+        if cls not in instances:
+            instances[cls] = cls(*args, **kw)
+        return instances[cls]
+
+    return _singleton
+
+
+@singleton
+class PrometheusClient:
+    """
+    A singleton class to ensure one instance of the class variables.
+    """
+    def __init__(self):
+        self.registry = CollectorRegistry()
+        self.counter = Counter('transform_method',
+                               'Transformation Pipeline',
+                               ['method', 'environment', 'transformation'],
+                               registry=self.registry)
+
+    def get_registry(self):
+        """
+        Get registry
+        """
+        return self.registry
+
+    def get_counter(self):
+        """
+        Get counter
+        """
+        return self.counter
+
+
+class OsirisFileClient(DataLakeFileClient):
+    """
+    OsirisFileClient is a synchronous file client wrapping the DataLakeFileClient.
+    This is to have metrics of Azure Storage transparent for the user.
+
+    Class requires the following configuration:
+    [Prometheus]
+    hostname = <hostname of Prometheus (aggregated) push gateway>
+    environment = <test/prod/dev>
+    name = <name of the transformation or module using it>
+    """
+    def __init__(self,
+                 account_url: str,
+                 file_system_name: str,
+                 file_path: str,
+                 credential: Optional[Any] = None,
+                 **kwargs: Any
+                 ):
+        super().__init__(account_url, file_system_name, file_path, credential, **kwargs)
+        configuration = ConfigurationWithCredentials(__file__)
+        config = configuration.get_config()
+        self.environment = config['Prometheus']['environment']
+        self.name = config['Prometheus']['name']
+        self.hostname = config['Prometheus']['hostname']
+        prometheus_registry = PrometheusClient()
+        self.counter = prometheus_registry.get_counter()
+        self.registry = prometheus_registry.get_registry()
+
+    def __del__(self):
+        if self.environment in ['prod', 'test']:
+            push_to_gateway(self.hostname, job='transform/dmi_weather', registry=self.registry)
+
+    def create_file(self,
+                    content_settings: Optional[ContentSettings] = None,
+                    metadata: Optional[Dict[str, str]] = None,
+                    **kwargs: Any):
+        self.counter.labels('create_file', self.environment, self.name).inc()
+        return super().create_file(content_settings, metadata, **kwargs)
+
+    def delete_file(self, **kwargs: Any):
+        self.counter.labels('delete_file', self.environment, self.name).inc()
+        return super().delete_file(**kwargs)
+
+    def upload_data(self, data: Union[AnyStr, Iterable[AnyStr], IO[AnyStr]],
+                    length: Optional[int] = None,
+                    overwrite: Optional[bool] = False,
+                    **kwargs):
+        self.counter.labels('upload_data', self.environment, self.name).inc()
+        return super().upload_data(data, length, overwrite, **kwargs)
+
+    def download_file(self,
+                      offset: Optional[int] = None,
+                      length: Optional[int] = None,
+                      **kwargs: Any) -> StorageStreamDownloader:
+        self.counter.labels('download_file', self.environment, self.name).inc()
+        return super().download_file(offset, length, **kwargs)
+
+    def rename_file(self, new_name: str, **kwargs: Any):
+        self.counter.labels('rename_file', self.environment, self.name).inc()
+        return super().rename_file(new_name, **kwargs)
+
+
+class OsirisFileClientAsync(DataLakeFileClientAsync):
+    """
+    OsirisFileClientAsync is a asynchronous file client wrapping the DataLakeFileClientAsync.
+    This is to have metrics of Azure Storage transparent for the user.
+
+    Class requires the following configuration:
+    [Prometheus]
+    hostname = <hostname of Prometheus (aggregated) push gateway>
+    environment = <test/prod/dev>
+    name = <name of the transformation or module using it>
+    """
+    def __init__(self,
+                 account_url: str,
+                 file_system_name: str,
+                 file_path: str,
+                 credential: Optional[Any] = None,
+                 **kwargs: Any
+                 ):
+        super().__init__(account_url, file_system_name, file_path, credential, **kwargs)
+        configuration = ConfigurationWithCredentials(__file__)
+        config = configuration.get_config()
+        self.environment = config['Prometheus']['environment']
+        self.name = config['Prometheus']['name']
+        self.hostname = config['Prometheus']['hostname']
+        prometheus_registry = PrometheusClient()
+        self.counter = prometheus_registry.get_counter()
+        self.registry = prometheus_registry.get_registry()
+
+    def __del__(self):
+        if self.environment in ['prod', 'test']:
+            push_to_gateway(self.hostname, job='transform/dmi_weather', registry=self.registry)
+
+    async def create_file(self,
+                          content_settings: Optional[ContentSettings] = None,
+                          metadata: Optional[Dict[str, str]] = None,
+                          **kwargs: Any):
+        self.counter.labels('create_file', self.environment, self.name).inc()
+        return await super().create_file(content_settings, metadata, **kwargs)
+
+    async def delete_file(self, **kwargs: Any):
+        self.counter.labels('delete_file', self.environment, self.name).inc()
+        return await super().delete_file(**kwargs)
+
+    async def upload_data(self,
+                          data: Union[AnyStr, Iterable[AnyStr], IO[AnyStr]],
+                          length: Optional[int] = None,
+                          overwrite: Optional[bool] = False,
+                          **kwargs: Any):
+        self.counter.labels('upload_data', self.environment, self.name).inc()
+        return await super().upload_data(data, length, overwrite, **kwargs)
+
+    async def download_file(self,
+                            offset: Optional[int] = None,
+                            length: Optional[int] = None,
+                            **kwargs: Any) -> StorageStreamDownloaderAsync:
+        self.counter.labels('download_file', self.environment, self.name).inc()
+        return await super().download_file(offset, length, **kwargs)
+
+    async def rename_file(self,
+                          new_name: str,
+                          **kwargs: Any):
+        self.counter.labels('rename_file', self.environment, self.name).inc()
+        return await super().rename_file(new_name, **kwargs)
